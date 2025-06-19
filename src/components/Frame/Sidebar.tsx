6da8cdb54c009d3169f54e99954216429f0950b0
quangtho908 <quangtho23062002@gmail.com>
2025-06-19 12:12:39 +0700

    feat(ui) update sidebar

--- src/components/Frame/Sidebar.tsx
+++ src/components/Frame/Sidebar.tsx
@@ -2,7 +2,6 @@
 import {Gamepad2, Home, ShoppingCart, Users, Package, BarChart3, Settings} from "lucide-react";
 import {useState} from "react";
 import {useSidebarStore} from "@/services/sidebarService";
-import {useRouter} from "next/navigation";
 import {useUserStore} from "@/services/user/userService";
 import _ from "lodash";
 
@@ -13,18 +12,17 @@ const menuItems = [
   { id: 'orders', label: 'Đơn hàng', icon: ShoppingCart, href: '/don-hang' },
   { id: 'employees', label: 'Nhân viên', icon: Users, href: '/nhan-vien', role: "ADMIN" },
   { id: 'analytics', label: 'Thống kê', icon: BarChart3, href: '/thong-ke', role: "ADMIN" },
-  { id: 'settings', label: 'Cài đặt', icon: Settings, href: '/settings', role: "ADMIN" },
+  // { id: 'settings', label: 'Cài đặt', icon: Settings, href: '/settings', role: "ADMIN" },
 ];
 
 export default function Sidebar() {
   const [activeItem, setActiveItem] = useState('dashboard');
   const { isOpen, closeSidebar } = useSidebarStore();
-  const router = useRouter();
   const {user} = useUserStore();
   const handleMenuClick = (itemId: string, href: string) => {
     setActiveItem(itemId);
     closeSidebar();
-    router.push(href)
+    window.location.href = href
   };
   return (
     <aside className={`
@@ -60,7 +58,7 @@ export default function Sidebar() {
                 <button
                   onClick={() => handleMenuClick(item.id, item.href)}
                   className={`
-                      w-full flex items-center space-x-3 px-3 py-2.5 rounded-lg text-left transition-colors duration-200
+                      cursor-pointer w-full flex items-center space-x-3 px-3 py-2.5 rounded-lg text-left transition-colors duration-200
                       ${isActive
                     ? 'bg-blue-50 text-blue-700 border-r-2 border-blue-700'
                     : 'text-gray-700 hover:bg-gray-50 hover:text-gray-900'
